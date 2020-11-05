51d6148dc6b7b194d6d5994f88907aa6ae809740
vaibhavdhabekar <vaibhavdhabekar1@hotmail.com>
2020-11-05 14:43:36 +0530

    vaibhav has commented

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace BooksApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")]//i am vaibhav//
     [ApiController]
     public class BooksController : ControllerBase
     {
